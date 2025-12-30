20e711bdb1928367f21fc62a5fa9629ee7c922d0
Nel <nelsonakaffou@yahoo.fr>
2025-12-30 16:55:07 +0100

    30122025 Up2

--- components/header.js
+++ components/header.js
@@ -7,14 +7,14 @@ class Header extends HTMLElement {
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-        <h1><a class="navbar-brand" href="pages/index.html">Nelson AKAFFOU</a></h1>
+        <h1><a class="navbar-brand" href="/index.html">Nelson AKAFFOU</a></h1>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-collapse collapse" id="navbarCollapse" style="">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/">Home</a>
+              <a class="nav-link" href="/index.html">Home</a>
              </li>
                       
             
