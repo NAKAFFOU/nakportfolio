3a1cc7a673b40a5154b8d468caf793b125b560a4
Nel <nelsonakaffou@yahoo.fr>
2025-12-30 17:19:52 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,38 +1,37 @@
 class Header extends HTMLElement {
-  constructor() {
-    super();
-  }
-
   connectedCallback() {
+    const inPagesFolder = window.location.pathname.includes("/pages/");
+    const PREFIX = inPagesFolder ? "../" : "";
+
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-        <h1><a class="navbar-brand" href="/index.html">Nelson AKAFFOU</a></h1>
-        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbarCollapse" style="">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/index.html">Home</a>
-             </li>
-                      
-            
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="pages/modules.html">MSC Enterprise IT Management</a>
-            </li>
-            
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="pages/experience.html">Experience</a>
-            </li>
-          
+          <h1><a class="navbar-brand" href="${PREFIX}index.html">Nelson AKAFFOU</a></h1>
 
-          </ul>
-        </div>
+          <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="navbar-collapse collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="${PREFIX}index.html">Home</a>
+              </li>
+
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="${PREFIX}pages/modules.html">MSC Enterprise IT Management</a>
+              </li>
+
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="${PREFIX}pages/experience.html">Experience</a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
     `;
   }
 }
 
-customElements.define('header-component', Header);
+customElements.define("header-component", Header);
